5a1963b7f834515966db89bd1f166d0460b648fb
Sreejesh <sreejeshsj01@gmail.com>
2025-06-09 14:31:56 +0530

    click event

--- src/course/Course.jsx
+++ src/course/Course.jsx
@@ -1,12 +1,15 @@
 import "./course.css";
 
 function Course({ name, price, image, rating }) {
+  function buttonClick(discount){
+    console.log(name+" "+'purchased '+  discount +" % Discount")
+  }
   return (
     name && <div className="card">
       <h2>{name}</h2>
       <img src={image} alt={name} width="100" />
       <p>Price: {price}</p>
-      <p>Rating: {rating}/10</p>
+      <button onClick={()=>buttonClick('20')}>Buy now</button>
     </div>
   )
 }
