a218d2158fd53340eb21920d49b055c4c93ae632
Sreejesh <sreejeshsj01@gmail.com>
2025-06-13 16:32:42 +0530

    useRef

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,18 +1,19 @@
-import React, { useState,useContext } from 'react'
+import React, { useState,useContext,useRef } from 'react'
 import {dataContext} from './Home'
 function Counter() {
     const [count,setCount]=useState(0)
     const data=useContext(dataContext)
+    let refCounter=useRef(0)
     function counter(){
 
-        setCount(preCount=>preCount+1)
-        setCount(preCount=>preCount+1)
+      refCounter.current++
     }
   return (
     <div>
       <h1>{data}</h1>
       <h3 className='m-5'>{count}</h3>
       <button onClick={counter}>Up</button>
+      <button onClick={()=>setCount(refCounter.current)}>Update</button>
     </div>
   )
 }
